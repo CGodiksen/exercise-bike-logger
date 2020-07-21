1fe2035d97d1a6c5e7bbd6b05e5f083123eb201e
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-07-21 03:09:34 +0200

    Since the live workout page is only relevant during a workout it would make no sense for it to be viewable under other circumstances. For that reason the live workout page is now only opened when the "New workout" button is pressed on the main window.

--- main_window.py
+++ main_window.py
@@ -1,11 +1,6 @@
-import asyncio
-import time
+from workout_window import WorkoutWindow
 
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtCore import QThreadPool
-
-import bluetooth_session
-from worker import Worker
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -15,41 +10,6 @@ class MainWindow(QtWidgets.QMainWindow):
         # Load the UI Page
         uic.loadUi("resources/mainwindow.ui", self)
 
-        # Setting up multi threading.
-        self.threadpool = QThreadPool()
-
-        # TODO Find a way to avoid these being hardcoded.
-        # The characteristic that should be written to.
-        self.characteristic_uuid = "***REMOVED***"
-
-        self.address = "***REMOVED***"
-        self.loop = asyncio.get_event_loop()
-
-        # Connecting buttons to their corresponding functionality.
-        self.startWorkoutButton.clicked.connect(self.start_workout)
-
-    def start_workout(self):
-        """Setting up the BluetoothSession object and starting the session when the "Start workout" btn is pressed."""
-        # Using Epoch time as the filename to ensure that each workout session has an unique filename.
-        filename = f"{time.time():.0f}"
-
-        bx70i = bluetooth_session.BluetoothSession(self.characteristic_uuid, self.address, self.loop, filename, 13,
-                                                   "00:01:00", self.update_live_page)
-        worker = Worker(self.loop.run_until_complete, bx70i.start_session())
-        self.threadpool.start(worker)
-
-    def update_live_page(self, data):
-        """
-        Updating the display widgets on the live workout page with the newest data every second. This method is called
-        every time we process a new data response from the exercise bike to ensure the newest data is displayed.
-
-        :param data: The data that should be used to update the display widgets.
-        """
-        self.timeLabel.setText(data[0])
-        self.speedNumber.display(data[1])
-        self.rpmNumber.display(data[2])
-        self.distanceNumber.display(data[3])
-        self.caloriesNumber.display(data[4])
-        self.heartRateNumber.display(data[5])
-        self.wattNumber.display(data[6])
-        self.levelSpinBox.setValue(int(data[7]))
+        # Setting up the live workout window that is opened when the "New workout" button is pressed.
+        self.workout_window = WorkoutWindow()
+        self.newWorkoutButton.clicked.connect(self.workout_window.show)
