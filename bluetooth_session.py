af4fabe63d652eeca4837128845499cd7b10b23d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-07-25 04:16:05 +0200

    Heavily refactored the BluetoothSession class to avoid having one method doing many tasks. This refactoring also should allow us to set the resistance level during the workout which is the first step towards implementing workout programs.

--- bluetooth_session.py
+++ bluetooth_session.py
@@ -26,7 +26,7 @@ class BluetoothSession:
     One BluetoothSession object represents one workout session.
     """
 
-    def __init__(self, characteristic_uuid, address, loop, filename, level, duration, display_updater):
+    def __init__(self, characteristic_uuid, address, loop, filename, initial_level, duration, display_updater):
         """
         Method called when the Bluetooth object is initialized.
 
@@ -34,7 +34,7 @@ class BluetoothSession:
         :param address: The MAC address of the device that we want to connect with.
         :param loop: The asyncio event loop that should be used by the BleakClient.
         :param filename: The CSV file in which the data should be saved.
-        :param level: The resistance level that is chosen for this specific workout.
+        :param initial_level: The resistance level that is chosen for this specific workout.
         :param duration: The total duration of the workout session as a string with the format "HH:MM:SS".
         :param display_updater: The function that updates the display widgets on the live workout page. This is called
         every time the data from a READ write response is processed.
@@ -43,63 +43,67 @@ class BluetoothSession:
         self.address = address
         self.loop = loop
         self.filename = filename
-        self.level = level
+        self.initial_level = initial_level
         self.duration = duration
         self.display_updater = display_updater
 
         # Flag that is set to True when the workout session is complete.
         self.stop_flag = False
 
-    # TODO: Split this method into multiple methods.
-    async def start_session(self):
+    async def run_session(self):
         """
         Connecting to the device that is associated with the given MAC address, configuring the resistance level of the
         workout, starting the workout session and reading data from the bike every second until it is finished.
         """
         try:
-            # A very specific protocol is followed to ensure that the connection with the exercise bike is initialized
-            # correctly.
             async with bleak.BleakClient(self.address, loop=self.loop) as client:
-                # Activating notifications on the characteristic that is written to.
-                await client.start_notify(self.characteristic_uuid, self.notification_handler)
+                await self.initialize_session(client)
 
-                await client.write_gatt_char(self.characteristic_uuid, PING)
-
-                await client.write_gatt_char(self.characteristic_uuid, INIT_A0)
-
-                for i in range(5):
-                    await client.write_gatt_char(self.characteristic_uuid, PING)
-
-                await client.write_gatt_char(self.characteristic_uuid, INIT_A3)
-
-                await client.write_gatt_char(self.characteristic_uuid, INIT_A4)
-
-                # TODO: Maybe make it possible to change the resistance level during the workout.
-                # Setting the resistance level for the workout session.
-                lvl = struct.pack('BBBBBB', 0xf0, 0xa6, 0x01, 0x01, self.level + 1,
-                                  (0xf0 + 0xa6 + 3 + self.level) & 0xFF)
-                await client.write_gatt_char(self.characteristic_uuid, lvl)
-
+                await self.set_level(client, self.initial_level)
                 time.sleep(0.2)
 
                 # Starting the workout session.
                 await client.write_gatt_char(self.characteristic_uuid, START)
-
-                time.sleep(0.1)
+                time.sleep(0.2)
 
                 while not self.stop_flag:
                     # Reading the current data from the exercise bike.
                     await client.write_gatt_char(self.characteristic_uuid, READ)
                     time.sleep(1)
 
-                # Stopping the session on the bike itself and deactivating notifications on the characteristic.
-                await client.write_gatt_char(self.characteristic_uuid, STOP)
-                await client.stop_notify(self.characteristic_uuid)
-
-                # Processing the entire workout session to extract further data.
-                data_processing.process_workout_session(self.filename)
+                await self.stop_session(client)
         except bleak.BleakError as e:
             print(f"Bleak raised an exception: {e}")
+
+    async def initialize_session(self, client):
+        """Running the specific initialization protocol used to connect to the exercise bike."""
+        # Activating notifications on the characteristic that is written to.
+        await client.start_notify(self.characteristic_uuid, self.notification_handler)
+
+        await client.write_gatt_char(self.characteristic_uuid, PING)
+
+        await client.write_gatt_char(self.characteristic_uuid, INIT_A0)
+
+        for i in range(5):
+            await client.write_gatt_char(self.characteristic_uuid, PING)
+
+        await client.write_gatt_char(self.characteristic_uuid, INIT_A3)
+
+        await client.write_gatt_char(self.characteristic_uuid, INIT_A4)
+
+    async def set_level(self, client, level):
+        """Setting the resistance level for the workout session."""
+        lvl = struct.pack('BBBBBB', 0xf0, 0xa6, 0x01, 0x01, level + 1, (0xf0 + 0xa6 + 3 + level) & 0xFF)
+        await client.write_gatt_char(self.characteristic_uuid, lvl)
+
+    async def stop_session(self, client):
+        """Closing the bluetooth connect and starting the data postprocessing."""
+        # Stopping the session on the bike itself and deactivating notifications on the characteristic.
+        await client.write_gatt_char(self.characteristic_uuid, STOP)
+        await client.stop_notify(self.characteristic_uuid)
+
+        # Processing the entire workout session to extract further data.
+        data_processing.process_workout_session(self.filename)
 
     def notification_handler(self, sender, data):
         """Handling the notifications that are received from a characteristic."""
