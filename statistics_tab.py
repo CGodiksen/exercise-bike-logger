b685b728ba04e29728641b29f6c52f583610c10f
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-07-31 04:41:24 +0200

    Implemented the function that updates the display on the statistics tab with the current statistics.

--- statistics_tab.py
+++ statistics_tab.py
@@ -11,9 +11,47 @@ class StatisticsTab:
         self.statistics = {}
 
         self.process_workouts()
+        self.update_display()
 
     def update_display(self):
         """Updates the labels in the statistics tab."""
+        self.main_window.totalWorkoutsLabel.setText(self.statistics["total_workouts"])
+        self.main_window.totalTimeLabel.setText(self.statistics["total_time"])
+        self.main_window.totalDistanceLabel.setText(self.statistics["total_distance"])
+        self.main_window.totalCaloriesLabel.setText(self.statistics["total_calories"])
+
+        self.main_window.longestWorkoutLabel.setText(self.statistics["longest_workout"][0])
+        self.main_window.longestWorkoutDateLabel.setText(self.statistics["longest_workout"][1])
+
+        self.main_window.longestDistanceLabel.setText(self.statistics["longest_distance"][0])
+        self.main_window.longestDistanceDateLabel.setText(self.statistics["longest_distance"][1])
+
+        self.main_window.mostCaloriesBurnedLabel.setText(self.statistics["most_calories_burned"][0])
+        self.main_window.mostCaloriesBurnedDateLabel.setText(self.statistics["most_calories_burned"][1])
+
+        self.main_window.highAvgSpeedLabel.setText(self.statistics["highest_average_speed"][0])
+        self.main_window.highAvgSpeedDateLabel.setText(self.statistics["highest_average_speed"][1])
+
+        self.main_window.highAvgRPMLabel.setText(self.statistics["highest_average_rpm"][0])
+        self.main_window.highAvgRPMDateLabel.setText(self.statistics["highest_average_rpm"][1])
+
+        self.main_window.highAvgHeartRateLabel.setText(self.statistics["highest_average_heart_rate"][0])
+        self.main_window.highAvgHeartRateDateLabel.setText(self.statistics["highest_average_heart_rate"][1])
+
+        self.main_window.highAvgWattLabel.setText(self.statistics["highest_average_watt"][0])
+        self.main_window.highAvgWattDateLabel.setText(self.statistics["highest_average_watt"][1])
+
+        self.main_window.highSpeedLabel.setText(self.statistics["highest_speed"][0])
+        self.main_window.highSpeedDateLabel.setText(self.statistics["highest_speed"][1])
+
+        self.main_window.highRPMLabel.setText(self.statistics["highest_rpm"][0])
+        self.main_window.highRPMDateLabel.setText(self.statistics["highest_rpm"][1])
+
+        self.main_window.highHeartRateLabel.setText(self.statistics["highest_heart_rate"][0])
+        self.main_window.highHeartRateDateLabel.setText(self.statistics["highest_heart_rate"][1])
+
+        self.main_window.highWattLabel.setText(self.statistics["highest_watt"][0])
+        self.main_window.highWattDateLabel.setText(self.statistics["highest_watt"][1])
 
     def update_graph(self):
         """Updates the graph in the statistics tab according to the chosen combo box configuration."""
@@ -32,18 +70,18 @@ class StatisticsTab:
                         data[header] = [value]
 
         # Calculating each statistic one by one using the data, starting with the total amount of workouts.
-        self.statistics["total_workouts"] = len(data["date"])
+        self.statistics["total_workouts"] = str(len(data["date"]))
 
         # Getting the total time in seconds and converting it to the format "DD day, HH:MM:SS".
         seconds = self.main_window.timestamps_to_seconds(data["duration"])
         self.statistics["total_time"] = str(datetime.timedelta(seconds=sum(seconds)))
 
-        self.statistics["total_distance"] = sum([float(i) for i in data["distance"]])
-        self.statistics["total_calories"] = sum([int(i) for i in data["calories"]])
+        self.statistics["total_distance"] = str(sum([float(i) for i in data["distance"]]))
+        self.statistics["total_calories"] = str(sum([int(i) for i in data["calories"]]))
 
         longest_workout_index = seconds.index(max(seconds))
-        self.statistics["longest_workout"] = (data["duration"][longest_workout_index],
-                                              data["date"][longest_workout_index])
+        date = datetime.datetime.fromtimestamp(int(data["date"][longest_workout_index])).strftime('%d-%m-%Y %H:%M:%S')
+        self.statistics["longest_workout"] = (str(data["duration"][longest_workout_index]), date)
 
         self.statistics["longest_distance"] = (self.get_max_value_date(data, "distance"))
         self.statistics["most_calories_burned"] = (self.get_max_value_date(data, "calories"))
@@ -70,4 +108,5 @@ class StatisticsTab:
 
         max_index = key_list.index(max(key_list))
 
-        return data[key][max_index], data["data"][max_index]
+        date = datetime.datetime.fromtimestamp(int(data["date"][max_index])).strftime('%d-%m-%Y %H:%M:%S')
+        return str(data[key][max_index]), date
