892967afd8d59a01ce8a8c42f02ea55bf70a749c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-08-03 22:44:47 +0200

    Added a dark theme.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import qdarkstyle
 
 from PyQt5 import QtWidgets
 
@@ -6,9 +8,20 @@ from main_window import MainWindow
 
 
 def main():
+    # Set the environment variable to use a specific wrapper.
+    # It can be set to PyQt, PyQt5, PySide or PySide2 (not implemented yet).
+    os.environ['PYQTGRAPH_QT_LIB'] = 'PyQt5'
+
+    # Import from pyqtgraph instead of doing it directly.
+    # Note that PyQtGraph always uses PyQt4 API.
+    from pyqtgraph.Qt import QtGui
+
     app = QtWidgets.QApplication(sys.argv)
 
     main_window = MainWindow()
+
+    # Setup stylesheet.
+    app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api=os.environ['PYQTGRAPH_QT_LIB']))
 
     main_window.show()
     sys.exit(app.exec_())
