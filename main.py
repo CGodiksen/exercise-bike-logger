42a07a60c08787247814ef4583714ea776d2d7f9
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-08-03 23:02:45 +0200

    Changed the dark theme since the previous one was kinda bad with pyqtgraph.

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-import os
 import sys
-import qdarkstyle
+import breeze_resources
 
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QFile, QTextStream
 
 from main_window import MainWindow
 
 
 def main():
-    # Set the environment variable to use a specific wrapper.
-    # It can be set to PyQt, PyQt5, PySide or PySide2 (not implemented yet).
-    os.environ['PYQTGRAPH_QT_LIB'] = 'PyQt5'
-
-    # Import from pyqtgraph instead of doing it directly.
-    # Note that PyQtGraph always uses PyQt4 API.
-    from pyqtgraph.Qt import QtGui
-
     app = QtWidgets.QApplication(sys.argv)
 
     main_window = MainWindow()
 
-    # Setup stylesheet.
-    app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api=os.environ['PYQTGRAPH_QT_LIB']))
+    # Set stylesheet.
+    file = QFile(":/dark.qss")
+    file.open(QFile.ReadOnly | QFile.Text)
+    stream = QTextStream(file)
+    app.setStyleSheet(stream.readAll())
 
     main_window.show()
     sys.exit(app.exec_())
